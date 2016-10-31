74e487f1504c03c987bd5c3968d4dda93aa4067e
Fredrik Svantesson <Fredik@Fredriks-Air.lan>
2016-10-31 22:48:28 +0100

    Parsed csv file and add cells

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,32 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Fredrik Svantesson on 2016-10-31.
+//  Copyright © 2016 Alsadevelopment. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Fredrik Svantesson on 2016-10-28.
+//  Copyright © 2016 Alsadevelopment. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
